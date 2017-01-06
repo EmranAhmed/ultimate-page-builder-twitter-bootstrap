1dd31e2361b1e795d25795d2a5f49900c2f2e83c
Emran Ahmed <emran.bd.08@gmail.com>
2017-01-06 23:26:57 +0600

    :wrench: Add Gulp file. :globe_with_meridians: add translation

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const gulp    = require('gulp');
+const plumber = require('gulp-plumber');
+const wpPot   = require('gulp-wp-pot');
+const sort    = require('gulp-sort'); // Recommended to prevent unnecessary changes in pot-file.
+
+const wpPotOptions = {
+    domain         : 'ultimate-page-builder-twitter-bootstrap',
+    package        : 'Ultimate Page Builder Twitter Bootstrap',
+    bugReport      : 'https://github.com/EmranAhmed/ultimate-page-builder-twitter-bootstrap/issues',
+    lastTranslator : 'ThemeHippo <themehippo@gmail.com>',
+    team           : 'ThemeHippo <themehippo@gmail.com>',
+
+    destFile      : 'ultimate-page-builder-twitter-bootstrap.pot',
+    translatePath : './languages'
+};
+
+// translate
+gulp.task('translate', () => {
+    return gulp.src(`./**/*.php`)
+        .pipe(sort())
+        .pipe(wpPot(wpPotOptions))
+        .pipe(gulp.dest(`${wpPotOptions.translatePath}/${wpPotOptions.destFile}`))
+});
+
+// yarn run build
+gulp.task('build', ['translate']);
--- ultimate-page-builder-twitter-bootstrap.php
+++ ultimate-page-builder-twitter-bootstrap.php
@@ -12,6 +12,7 @@
 
     defined( 'ABSPATH' ) or die( 'Keep Silent' );
 
+
     if ( ! class_exists( 'Ultimate_Page_Builder_Twitter_Bootstrap' ) ):
 
         class Ultimate_Page_Builder_Twitter_Bootstrap {
@@ -55,6 +56,44 @@
             }
 
             public function hooks() {
+                add_action( 'admin_notices', array( $this, 'php_requirement_notice' ) );
+                add_action( 'admin_notices', array( $this, 'upb_requirement_notice' ) );
+                add_action( 'init', array( $this, 'language' ) );
+            }
+
+            public function has_required_php_version() {
+                return version_compare( PHP_VERSION, '5.3.0', '>' );
+            }
+
+            public function php_requirement_notice() {
+                if ( ! $this->has_required_php_version() ) {
+                    $class   = 'notice notice-error';
+                    $text    = esc_html__( 'Please check PHP version requirement.', 'ultimate-page-builder-twitter-bootstrap' );
+                    $link    = esc_url( 'https://wordpress.org/about/requirements/' );
+                    $message = wp_kses( __( "It's required to use latest version of PHP to use <strong>Ultimate Page Builder Twitter Bootstrap</strong>.", 'ultimate-page-builder-twitter-bootstrap' ), array( 'strong' => array() ) );
+
+                    printf( '<div class="%1$s"><p>%2$s <a target="_blank" href="%3$s">%4$s</a></p></div>', $class, $message, $link, $text );
+                }
+            }
+
+            public function upb_requirement_notice() {
+                if ( ! $this->is_upb_installed() ) {
+                    $class = 'notice notice-error';
+
+                    $text    = esc_html__( 'Ultimate Page Builder', 'ultimate-page-builder-twitter-bootstrap' );
+                    $link    = esc_url( 'https://wordpress.org/plugins/ultimate-page-builder/' );
+                    $message = wp_kses( __( "<strong>Ultimate Page Builder: Twitter Bootstrap</strong> is an add-on of ", 'ultimate-page-builder-twitter-bootstrap' ), array( 'strong' => array() ) );
+
+                    printf( '<div class="%1$s"><p>%2$s <a target="_blank" href="%3$s"><strong>%4$s</strong></a></p></div>', $class, $message, $link, $text );
+                }
+            }
+
+            public function language() {
+                load_plugin_textdomain( 'ultimate-page-builder-twitter-bootstrap', FALSE, trailingslashit( UPB_TB_PLUGIN_DIRNAME ) . 'languages' );
+            }
+
+            public function is_upb_installed() {
+                return in_array( 'ultimate-page-builder/ultimate-page-builder.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) );
             }
         }
 
